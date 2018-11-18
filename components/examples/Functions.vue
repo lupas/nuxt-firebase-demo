e328850191e79fbe47a1b0e3b61c707bd5060d5b
Pascal <pascal@luther.ch>
2018-11-18 16:36:27 +0100

    added Firebase Functions implementation

--- components/examples/Functions.vue
+++ components/examples/Functions.vue
@@ -4,12 +4,25 @@
     <h3 class="sectionTitle">
       Firebase Functions
     </h3>
-    tbd
+    <div class="links">
+      <a class="button--green" @click="callTestFunction()">Call Test Function</a>
+      <p class="mt-1">Might take some seconds.</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  methods: {}
+  methods: {
+    async callTestFunction() {
+      try {
+        const res = await this.$fireFunc.httpsCallable('testFunction')()
+        alert(res.data.message)
+      } catch (e) {
+        alert(e)
+        return
+      }
+    }
+  }
 }
 </script>
