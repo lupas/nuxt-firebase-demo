f5bbae555c2cfe0e70941095a8aa1796b17b9119
Pascal <pascal@luther.ch>
2018-11-18 16:15:45 +0100

    added auth example

--- components/examples/Auth.vue
+++ components/examples/Auth.vue
@@ -4,12 +4,26 @@
     <h3 class="sectionTitle">
       Firebase Authentication
     </h3>
-    tbd
+    <div class="links">
+      <a class="button--green" @click="createUser()">Create User</a>
+      <p class="mt-1">This will throw a password error coming from Firebase, but proves the point.</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  methods: {}
+  methods: {
+    async createUser() {
+      try {
+        await this.$fireAuth.createUserWithEmailAndPassword(
+          'foo@fooooooooooooooooooo.com',
+          'test'
+        )
+      } catch (e) {
+        alert(e)
+      }
+    }
+  }
 }
 </script>
