6274a7497d9db6d2f9ad28270e53eb138ca4ace9
Pascal <pascal@luther.ch>
2019-10-22 15:01:37 +0800

    updated nuxtServerInit example

--- store/actions.js
+++ store/actions.js
@@ -1,5 +1,5 @@
 export default {
-  nuxtServerInit: function({}, ctx) {
+  nuxtServerInit({}, ctx) {
     if (this.$fireAuth === null) {
       throw 'nuxtServerInit Example not working - this.$fireAuth cannot be accessed.'
     }
@@ -8,17 +8,21 @@ export default {
       throw 'nuxtServerInit Example not working - ctx.$fireAuth cannot be accessed.'
     }
 
+    if (ctx.app.$fireAuth === null) {
+      throw 'nuxtServerInit Example not working - ctx.$fireAuth cannot be accessed.'
+    }
+
     console.info(
-      'Nuxt-fire Objects can be accessed in nuxtServerInit action via this.$fire___ and ctx.$fire___'
+      'Success. Nuxt-fire Objects can be accessed in nuxtServerInit action via this.$fire___, ctx.$fire___ and ctx.app.$fire___'
     )
   },
-  checkVuexStore: function({ commit, state, rootState }) {
+  checkVuexStore({ commit, state, rootState }) {
     if (this.$fireAuth === null) {
       throw 'Vuex Store example not working - this.$fireAuth cannot be accessed.'
     }
 
     alert(
-      'Nuxt-fire Objects can be accessed in store actions via this.$fire___'
+      'Success. Nuxt-fire Objects can be accessed in store actions via this.$fire___'
     )
     return
   }
