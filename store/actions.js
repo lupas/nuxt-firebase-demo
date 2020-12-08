05f9b4b7bcdb5c2f5fd896692af836e1e2d7b69a
Pascal Luther <pascal@luther.ch>
2020-12-09 00:20:54 +0100

    chore(cleanup): Removed not needed code

--- store/actions.js
+++ store/actions.js
@@ -1,20 +1,9 @@
 export default {
   async nuxtServerInit({ dispatch }, ctx) {
-    if (this.$fire.auth === null) {
-      throw 'nuxtServerInit Example not working - this.$fire.auth cannot be accessed.'
-    }
-
-    if (ctx.$fire.auth === null) {
-      throw 'nuxtServerInit Example not working - ctx.$fire.auth cannot be accessed.'
-    }
-
-    if (ctx.app.$fire.auth === null) {
-      throw 'nuxtServerInit Example not working - ctx.$fire.auth cannot be accessed.'
-    }
-
     // INFO -> Nuxt-fire Objects can be accessed in nuxtServerInit action via this.$fire___, ctx.$fire___ and ctx.app.$fire___'
 
     /** Get the VERIFIED authUser from the server */
+    console.log('USER', ctx.res.locals.user)
     if (ctx.res && ctx.res.locals && ctx.res.locals.user) {
       const { allClaims: claims, ...authUser } = ctx.res.locals.user
 
@@ -32,10 +21,18 @@ export default {
     }
   },
 
-  onAuthStateChanged({ commit }, { authUser }) {
+  async onAuthStateChanged({ commit }, { authUser }) {
     if (!authUser) {
       commit('RESET_STORE')
       return
+    }
+    if (authUser) {
+      try {
+        const idToken = await authUser.getIdToken(true)
+        console.log('idToken', idToken)
+      } catch (e) {
+        console.error(e)
+      }
     }
     commit('SET_AUTH_USER', { authUser })
   },
